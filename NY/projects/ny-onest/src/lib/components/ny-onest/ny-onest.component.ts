586e8e6a361d39ddc2301bedf47282d93f29676d
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2023-09-15 16:29:36 +0530

    code changes

--- NY/projects/ny-onest/src/lib/components/ny-onest/ny-onest.component.ts
+++ NY/projects/ny-onest/src/lib/components/ny-onest/ny-onest.component.ts
@@ -13,25 +13,195 @@ export class EyOnestComponent implements OnInit {
   searchQuery = '';
   baseUrl = '';
   toggleSearch: boolean = false;
-  formInit: boolean = true;
-  isContentInit: boolean = false;
+  formInit: boolean = false;
+  isContentInit: boolean = true;
   loading: boolean = false;
   selCardData: any;
   searchMessage: any;
   searchContext: any;
   // serachList;
   searchContentList: any[] = []
+  response:any;
+data={
+  "context": {
+      "action": "on_search",
+      "domain": "onest:financial-support",
+      "location": {
+          "country": {
+              "code": "IND"
+          }
+      },
+      "bpp_id": "pilot.vidyasaarathi.co.in",
+      "bpp_uri": "pilot.vidyasaarathi.co.in/path/to/your/api",
+      "bap_id": "nammayatri.in",
+      "bap_uri": "nammayatri.in/api/url",
+      "transaction_id": "whatever txn id namma yatri sent",
+      "message_id": "whatever msg id namma yatri sent"
+  },
+  "message": {
+      "catalog": {
+          "providers": [
+              {
+                  "id": "287363847",
+                  "descriptor": {
+                      "name": "JSW Foundation",
+                      "code": "jsw.in",
+                      "images": [
+                          {
+                              "url": "https://media.istockphoto.com/id/1220289026/vector/student-loan-color-icon-credit-to-pay-for-university-education-tuition-fee-college.jpg?s=1024x1024&w=is&k=20&c=j1CMGb4h9nqf9y8X0rRoVO1hFO6N3jE_6omwU3NxKt8=",
+                              "size": "sm"
+                          }
+                      ]
+                  },
+                  "items": [
+                      {
+                          "id": "827890",
+                          "descriptor": {
+                              "name": "Summer Fund for Bsc student April 2022"
+                          },
+                          "price": {
+                              "value": "1000 INR for one year"
+                          }
+                      }
+                  ],
+                  "fulfillments": [
+                      {
+                          "stops": [
+                              {
+                                  "type": "end",
+                                  "time": {
+                                      "timestamp": "2023-12-21 00:00:00"
+                                  }
+                              }
+                          ]
+                      }
+                  ]
+              },
+              {
+                  "id": "287363848",
+                  "descriptor": {
+                      "name": "Tata Foundation",
+                      "code": "tata.in",
+                      "images": [
+                          {
+                              "url": "https://media.istockphoto.com/id/1220289026/vector/student-loan-color-icon-credit-to-pay-for-university-education-tuition-fee-college.jpg?s=1024x1024&w=is&k=20&c=j1CMGb4h9nqf9y8X0rRoVO1hFO6N3jE_6omwU3NxKt8=",
+                              "size": "sm"
+                          }
+                      ]
+                  },
+                  "items": [
+                      {
+                          "id": "827890",
+                          "descriptor": {
+                              "name": "Summer Fund for BA student April 2022"
+                          },
+                          "price": {
+                              "value": "10000 INR for one year"
+                          }
+                      }
+                  ],
+                  "fulfillments": [
+                      {
+                          "stops": [
+                              {
+                                  "type": "end",
+                                  "time": {
+                                      "timestamp": "2023-12-21 00:00:00"
+                                  }
+                              }
+                          ]
+                      }
+                  ]
+              },
+              {
+                  "id": "287363848",
+                  "descriptor": {
+                      "name": "Jindal Foundation",
+                      "code": "jindal.in",
+                      "images": [
+                          {
+                              "url": "https://media.istockphoto.com/id/1220289026/vector/student-loan-color-icon-credit-to-pay-for-university-education-tuition-fee-college.jpg?s=1024x1024&w=is&k=20&c=j1CMGb4h9nqf9y8X0rRoVO1hFO6N3jE_6omwU3NxKt8=",
+                              "size": "sm"
+                          }
+                      ]
+                  },
+                  "items": [
+                      {
+                          "id": "827890",
+                          "descriptor": {
+                              "name": "Metric scholarship"
+                          },
+                          "price": {
+                              "value": "1000 INR per month for one year"
+                          }
+                      }
+                  ],
+                  "fulfillments": [
+                      {
+                          "stops": [
+                              {
+                                  "type": "end",
+                                  "time": {
+                                      "timestamp": "2023-12-21 00:00:00"
+                                  }
+                              }
+                          ]
+                      }
+                  ]
+              },
+              {
+                  "id": "287363841",
+                  "descriptor": {
+                      "name": "Tesla Foundation",
+                      "code": "tesla.in",
+                      "images": [
+                          {
+                              "url": "https://media.istockphoto.com/id/1220289026/vector/student-loan-color-icon-credit-to-pay-for-university-education-tuition-fee-college.jpg?s=1024x1024&w=is&k=20&c=j1CMGb4h9nqf9y8X0rRoVO1hFO6N3jE_6omwU3NxKt8=",
+                              "size": "sm"
+                          }
+                      ]
+                  },
+                  "items": [
+                      {
+                          "id": "827890",
+                          "descriptor": {
+                              "name": "Winter scholarship for engineering students"
+                          },
+                          "price": {
+                              "value": "5000 per year"
+                          }
+                      }
+                  ],
+                  "fulfillments": [
+                      {
+                          "stops": [
+                              {
+                                  "type": "end",
+                                  "time": {
+                                      "timestamp": "2023-12-21 00:00:00"
+                                  }
+                              }
+                          ]
+                      }
+                  ]
+              }
+          ]
+      }
+  }
+}
+
   constructor(private http: HttpClient, private searchService: SearchService) {
 
   }
 
   ngOnInit(): void {
-    this.searchMessage = {};
-    this.searchContext = {};
+    // this.searchMessage = {};
+    // this.searchContext = {};
+    this.searchContentList=this.data.message.catalog.providers
   }
   searchBasedQuery() {
     console.log('searchBasedQuery', this.searchQuery)
-    this.onBAPSearchCall();
+    //this.onBAPSearchCall();
   }
   openSearch() {
     this.toggleSearch = true;
@@ -78,52 +248,52 @@ export class EyOnestComponent implements OnInit {
   //   }
   //   );
   // }
-  onBAPSearchCall() {
-    let itemsList;
-    let providerName ='';
-    this.searchContentList = [];
-    this.loading = true;
-    this.http.get('https://staging.sunbirded.org/onest/bap/search?keyword=' + this.searchQuery).subscribe((res: any) => {
-      itemsList = res.data.filter((resData:any) => resData?.message?.catalog?.providers[0].id === "sunbird-ed-bpp")
-      this.searchContext = res?.data?.context;
-      this.searchMessage = res?.data?.message;
-      if (itemsList?.length || itemsList !== undefined) {
-        providerName = itemsList[0]?.message?.catalog?.providers[0]?.id;
-        itemsList = itemsList[0]?.message?.catalog?.providers[0]?.items;
-        console.log('provider:' , providerName);
-        itemsList.map((contentRes: any) => {
-          let mimeType = _.find(contentRes?.tags[0]?.list, (o: any)  => { return o.descriptor?.name == "mimeType"; })['value']
-          let content = {
-            identifier: contentRes?.id,
-            title: contentRes?.descriptor?.name,
-            subTitle: contentRes?.descriptor?.sort_desc,
-            img: contentRes?.descriptor?.images[0]?.url,
-            price: contentRes.price.currency + ' : ' + contentRes.price.value,
-            itemId: contentRes?.id,
-            artifactUrl: contentRes?.descriptor?.media,
-            mimeType,
-            provider:providerName,
-            tag:contentRes?.tags[0]?.list
-          }
-          this.searchContentList.push(content);
-        })
-        this.loading = false;
-      }
-      else {
-        this.loading = false;
-     //   this.onestSnackBar.open('No Provider found !!!','Close', {
-        //   duration: 2000, 
-        // });
+  // onBAPSearchCall() {
+  //   let itemsList;
+  //   let providerName ='';
+  //   this.searchContentList = [];
+  //   this.loading = true;
+  //   this.http.get('https://staging.sunbirded.org/onest/bap/search?keyword=' + this.searchQuery).subscribe((res: any) => {
+  //     itemsList = res.data.filter((resData:any) => resData?.message?.catalog?.providers[0].id === "sunbird-ed-bpp")
+  //     this.searchContext = res?.data?.context;
+  //     this.searchMessage = res?.data?.message;
+  //     if (itemsList?.length || itemsList !== undefined) {
+  //       providerName = itemsList[0]?.message?.catalog?.providers[0]?.id;
+  //       itemsList = itemsList[0]?.message?.catalog?.providers[0]?.items;
+  //       console.log('provider:' , providerName);
+  //       itemsList.map((contentRes: any) => {
+  //         let mimeType = _.find(contentRes?.tags[0]?.list, (o: any)  => { return o.descriptor?.name == "mimeType"; })['value']
+  //         let content = {
+  //           identifier: contentRes?.id,
+  //           title: contentRes?.descriptor?.name,
+  //           subTitle: contentRes?.descriptor?.sort_desc,
+  //           img: contentRes?.descriptor?.images[0]?.url,
+  //           price: contentRes.price.currency + ' : ' + contentRes.price.value,
+  //           itemId: contentRes?.id,
+  //           artifactUrl: contentRes?.descriptor?.media,
+  //           mimeType,
+  //           provider:providerName,
+  //           tag:contentRes?.tags[0]?.list
+  //         }
+  //         this.searchContentList.push(content);
+  //       })
+  //       this.loading = false;
+  //     }
+  //     else {
+  //       this.loading = false;
+  //    //   this.onestSnackBar.open('No Provider found !!!','Close', {
+  //       //   duration: 2000, 
+  //       // });
       
-      }
+  //     }
 
-    }, (error) => {
-      console.log(error);
-      this.loading = false; 
+  //   }, (error) => {
+  //     console.log(error);
+  //     this.loading = false; 
 
-    }
-    );
-  }
+  //   }
+  //   );
+  // }
   // onBAPInitCall(cardData: any) {
   //   let resBody = {
   //     //sample
